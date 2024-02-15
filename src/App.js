b495f54c5f71408fbec83550d1458c947fab5565
chanduccbp <kumarchandrasekhar57@gmail.com>
2024-02-15 12:31:10 +0000

    nxt watch app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {Component} from 'react'
 import {Switch, Route} from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
 import Login from './components/Login'
@@ -6,12 +7,18 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 // Replace your code here
-const App = () => (
-  <Switch>
-    <Route exact path="/login" component={Login} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <Route component={NotFound} />
-  </Switch>
-)
+class App extends Component {
+  state: {isLightTheme: true, activeTab: 'HOME'}
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    )
+  }
+}
 
 export default App
