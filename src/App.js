6548d839556ff42adcc8917ea05964cff6944c7c
chanduccbp <kumarchandrasekhar57@gmail.com>
2024-02-16 11:50:13 +0000

    nxt watch app

--- src/App.js
+++ src/App.js
@@ -4,19 +4,32 @@ import ProtectedRoute from './components/ProtectedRoute'
 import Login from './components/Login'
 import Home from './components/Home'
 import NotFound from './components/NotFound'
+import ThemeContext from './context/ThemeContext'
 import './App.css'
 
 // Replace your code here
 class App extends Component {
   state: {isLightTheme: true, activeTab: 'HOME'}
 
+  changeTheme = () => {
+    this.setState(prevState => ({
+      isLightTheme: !prevState.isLightTheme,
+    }))
+  }
+
   render() {
+    const {isLightTheme, activeTab} = this.state
+
     return (
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <ProtectedRoute exact path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
+      <ThemeContext.Provider
+        value={{isLightTheme, activeTab, changeTheme: this.changeTheme}}
+      >
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <ProtectedRoute exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </ThemeContext.Provider>
     )
   }
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,110 @@
+import {Link, withRouter} from 'react-router-dom'
+import {BsMoon} from 'react-icons/bs'
+import {FiSun, FiLogOut} from 'react-icons/fi'
+import Cookies from 'js-cookie'
+import Popup from 'reactjs-popup'
+import ThemeContext from '../../context/ThemeContext'
 
+import {
+  Navbar,
+  AppLogo,
+  LinkItem,
+  AppControls,
+  ThemeButton,
+  ProfilePic,
+  CustomButton,
+  LogoutContainer,
+  LogoutText,
+  ButtonsContainer,
+  LogoutIconButton,
+} from './styledComponents'
 
-const Header = () => {}
+const Header = props => {
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token')
+    const {history} = props
+    history.replace('/login')
+  }
 
-export default Header
+  return (
+    <ThemeContext.Consumer>
+      {value => {
+        const {isLightTheme, changeTheme} = value
+
+        const onClickingTheme = () => {
+          changeTheme()
+        }
+
+        return (
+          <Navbar>
+            <LinkItem to="/">
+              <AppLogo
+                src={
+                  isLightTheme
+                    ? 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png'
+                    : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png'
+                }
+                alt="website logo"
+              />
+            </LinkItem>
+            <AppControls>
+              <ThemeButton
+                type="button"
+                onClick={onClickingTheme}
+                data-testid="theme"
+              >
+                {isLightTheme ? <BsMoon /> : <FiSun />}
+              </ThemeButton>
+              <ProfilePic
+                src="https://assets.ccbp.in/frontend/react-js/nxt-watch-profile-img.png"
+                alt="profile"
+              />
+              <Popup
+                modal
+                trigger={<CustomButton type="button">Logout</CustomButton>}
+              >
+                {close => (
+                  <LogoutContainer>
+                    <LogoutText>Are you sure,you want to logout?</LogoutText>
+                    <ButtonsContainer>
+                      <CustomButton type="button" onClick={() => close()}>
+                        Cancel
+                      </CustomButton>
+                      <CustomButton type="button" onClick={onClickLogout}>
+                        Confirm
+                      </CustomButton>
+                    </ButtonsContainer>
+                  </LogoutContainer>
+                )}
+              </Popup>
+              <Popup
+                modal
+                trigger={
+                  <LogoutIconButton type="button">
+                    <FiLogOut />
+                  </LogoutIconButton>
+                }
+              >
+                {close => (
+                  <LogoutContainer>
+                    <LogoutText>Are you sure,you want to logout?</LogoutText>
+                    <ButtonsContainer>
+                      <CustomButton type="button" onClick={() => close()}>
+                        Cancel
+                      </CustomButton>
+                      <CustomButton type="button" onClick={onClickLogout}>
+                        Confirm
+                      </CustomButton>
+                    </ButtonsContainer>
+                  </LogoutContainer>
+                )}
+              </Popup>
+            </AppControls>
+          </Navbar>
+        )
+      }}
+    </ThemeContext.Consumer>
+  )
+}
+
+export default withRouter(Header)
